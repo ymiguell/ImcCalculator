1ee1ed5220b17457beaf272d6f8917be8b37d1ef
ymiguell <miguelmelo.o.fut@gmail.com>
2024-08-25 16:20:36 -0300

    Calculator

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Title from "@/src/components/Title";
+import Form from "@/src/components/Form"
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Title/>
+      <Form/>
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e0e5e5",
+    paddingTop: 80
+  }
+})
